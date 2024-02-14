754e8d60314c4ca10ea606f0ac329c9415113e34
Ivan Obraztsov <ivan-obraztsov@ya.ru>
2024-02-14 20:22:18 +0400

    completed?

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { processGeojson } from '@kepler.gl/processors';
 import { enhanceReduxMiddleware } from '@kepler.gl/reducers/dist';
 import searchIcon from './searchIcon.svg';
 import spinner from './spinner.svg';
+import axios from 'axios';
 
 const initialState = {};
 const middlewares = enhanceReduxMiddleware([
@@ -34,10 +35,21 @@ function App() {
 
 export default App;
 
+function getRandomInt(max: number): number {
+  return Math.floor(Math.random() * max);
+}
+
+const id = getRandomInt(10000000000);
+
+const auth = btoa(
+  `${'66e9f14f32b04a3daf9b15a5ac1046f8'}:${'13feda59-c00e-4506-a30f-2f9ef1d14689'}`,
+);
+
 const Map = () => {
   const dispatch = useDispatch();
   const [value, setValue] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [rows, setRows] = useState<string[]>([]);
 
   useEffect(() => {
     dispatch(
@@ -55,7 +67,49 @@ const Map = () => {
   }, []);
 
   const searchData = (): void => {
-    setLoading(!loading);
+    setLoading(true);
+    axios
+      .post(
+        'https://core.malevich.ai/api/v1/endpoints/run/ee4f3b5757c8b7cf0019182c77a23ff07e90c4f465a6ba1260d7f0387d6e82c3',
+        {
+          cfg: {
+            rawCollections: {
+              df: [
+                ...rows,
+                `{"id": ${rows?.length}, "turn": ${id}, "geo_json": "", "user_message": "${value}"}`,
+              ],
+            },
+          },
+        },
+        {
+          headers: {
+            Authorization: 'Basic ' + auth,
+          },
+        },
+      )
+      .then((res) => {
+        setRows((prev) => [
+          ...prev,
+          `{"id": ${rows?.length}, "turn": ${id}, "geo_json": "", "user_message": ${value}}`,
+        ]);
+        console.log(res);
+        dispatch(
+          addDataToMap({
+            datasets: {
+              info: {
+                label: value,
+                id: value,
+              },
+              // @ts-expect-error data correct
+              data: processGeojson(res),
+            },
+          }),
+        );
+        setLoading(false);
+      })
+      .catch(() => {
+        // setLoading(false);
+      });
   };
 
   return (
